c068686bdffa797334182e04dc089758dc9c8e9f
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2021-12-04 14:35:38 +0700

    add Hello

---
+++ IntroductionJava/Hello/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
