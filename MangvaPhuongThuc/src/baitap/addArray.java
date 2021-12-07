2384b942fe8f066245b8847cdbf829be0441a330
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2021-12-07 14:50:32 +0700

    add gop mang

---
+++ MangvaPhuongThuc/src/baitap/addArray.java
@@ -0,0 +1,24 @@
+package baitap;
+
+import java.util.Arrays;
+
+public class addArray {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {3, 4, 5};
+        int length = array1.length + array2.length;
+        int[] result = new int[length];
+        int pos = 0;
+        for (int element : array1) {
+            result[pos] = element;
+            pos++;
+        }
+        for (int element : array2) {
+            result[pos] = element;
+            pos++;
+        }
+
+        System.out.println("Mang moi sau khi gop la: ");
+        System.out.println(Arrays.toString(result));
+    }
+}
