f347979aac4f4df23bc6df7249644a28b07ea1bb
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2021-12-09 16:10:55 +0700

    add minitest1

---
+++ MiniTest1/src/Minitest/Main.java
@@ -0,0 +1,47 @@
+package Minitest;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap so luong san pham: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("San pham moi: ");
+        Product[] productList = new Product[n];
+
+        for (int i = 0; i < productList.length; i++) {
+            System.out.println("Nhap thong tin cua san pham thu " + (i + 1));
+            productList[i] = product(scanner);
+        }
+
+        for (int i = 0; i < productList.length; i++) {
+            System.out.println(productList[i].toString());
+        }
+
+        double sumProduct = 0;
+        for (int i = 0; i < productList.length; i++) {
+            sumProduct += productList[i].getPrice();
+        }
+        System.out.println("Tong gia san pham la: " + sumProduct);
+    }
+
+    public static Product product(Scanner scanner) {
+        int code;
+        String name;
+        double price;
+        String type;
+
+        System.out.println("Nhap code: ");
+        code = scanner.nextInt();
+        System.out.println("Nhap ten: ");
+        name = scanner.nextLine();
+        System.out.println("Nhap gia: ");
+        price = scanner.nextDouble();
+        System.out.println("Nhap loai: ");
+        type = scanner.nextLine();
+
+        return new Product(code, name, price, type);
+    }
+}
---
+++ MiniTest1/src/Minitest/Product.java
@@ -0,0 +1,63 @@
+package Minitest;
+
+public class Product {
+    private int code;
+    private String name;
+    private double price;
+    private String type;
+    static String unit = "USD";
+
+    public Product() {
+
+    }
+
+    public Product(int code, String name, double price, String type) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", price=" + price +
+                ", unit='" + unit + '\'' +
+                '}';
+    }
+}
