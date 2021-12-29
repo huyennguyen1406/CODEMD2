36823bdb097bc24beb2faf5c66e3fafa65b4a81d
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2021-12-29 23:47:03 +0700

    add bt thuc hanh

--- Exercise/src/StudentManager/main/Main.java
+++ Exercise/src/StudentManager/main/Main.java
@@ -3,73 +3,63 @@ package StudentManager.main;
 import StudentManager.manager.StudentManager;
 import StudentManager.model.Student;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    private static final String PATH_NAME = "src/StudentManager/students";
-
     public static void main(String[] args) {
-        StudentManager studentManager = null;
-        try {
-            studentManager = new StudentManager(PATH_NAME);
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println(e.getMessage());
-        }
-
         Scanner scanner = new Scanner(System.in);
+        StudentManager studentManager = new StudentManager();
 
         int choice;
         do {
             System.out.println("MENU:");
             System.out.println("1. Them 1 sinh vien");
-            System.out.println("2. Xoa 1 sinh vien theo ten");
-            System.out.println("3. Sua sinh vien theo ten");
+            System.out.println("2. Sua 1 sinh vien theo ten");
+            System.out.println("3. Xoa sinh vien theo ten");
             System.out.println("4. Hien thi tat ca sinh vien");
             System.out.println("5. Hien thi tat ca sinh vien co diem trung binh tren 7.5");
             System.out.println("6. Hien thi sinh vien theo cu phap");
+            System.out.println("7. Ghi file");
+            System.out.println("8. Doc file");
             System.out.println("0. Exit");
             System.out.println("Enter your choice");
             choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
                 case 1:
-                    Student student = studentManager.createStudent();
-                    if (studentManager.addStudent(student)) {
-                        System.out.println("Thêm thành công");
-                    } else {
-                        System.out.println("Thêm không thành công");
-                    }
+                    studentManager.addStudent();
                     break;
                 case 2:
-                    System.out.println("Nhập id muốn xóa: ");
-                    String deleteName = scanner.nextLine();
-                    Student studentDelete = studentManager.deleteStudent(deleteName);
-                    if (studentDelete != null) {
-                        System.out.println("Xóa thành công");
-                        System.out.println(studentDelete);
-                    } else {
-                        System.out.println("Xóa thất bại");
-                    }
+                    System.out.println("Nhập ten muốn sua: ");
+                    scanner.nextLine();
+                    String editName = scanner.nextLine();
+                    System.out.println(studentManager.editStudent(editName));
                     break;
                 case 3:
-                    System.out.println("Mhap ten sinh vien can sua:");
-                    String name1 = scanner.nextLine();
-                    Student studentEdit = studentManager.updateStudent(name1);
-                    if (studentEdit != null) {
-                        System.out.println("Sửa thành công");
-                        System.out.println(studentEdit);
-                    } else {
-                        System.out.println("Sửa không thành công");
-                    }
+                    System.out.println("Mhap ten sinh vien can xoa:");
+                    scanner.nextLine();
+                    String deleteName = scanner.nextLine();
+                    System.out.println(studentManager.deleteStudent(deleteName));
                     break;
                 case 4:
-                    studentManager.displayStudent();
+                    System.out.println("Tat ca sinh vien: ");
+                    studentManager.displayAll();
                     break;
                 case 5:
                     System.out.println("Danh sach sinh vien tren 7.5");
-                    studentManager.averagePointHigher();
+                    studentManager.displayStudentByGpa();
                     break;
+                case 6:
+                    System.out.println("Tat ca sinh vien theo format: ");
+                    studentManager.displayStudentFormat();
+                    break;
+                case 7:
+                    studentManager.writeFile(studentManager.getStudents(), StudentManager.PATH_NAME);
+                    break;
+                case 8:
+                    ArrayList<Student> students = studentManager.readFile(StudentManager.PATH_NAME);
+                    students.forEach(System.out::println);
                     break;
             }
         } while (choice != 0);
--- Exercise/src/StudentManager/manager/StudentManager.java
+++ Exercise/src/StudentManager/manager/StudentManager.java
@@ -2,95 +2,198 @@ package StudentManager.manager;
 
 import StudentManager.model.Student;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class StudentManager {
-    ArrayList<Student> students = new ArrayList<>();
-    Scanner scanner = new Scanner(System.in);
+    public static final String PATH_NAME = "src/StudentManager/students.csv";
 
-    public Student createStudent() {
+    private final ArrayList<Student> students;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public StudentManager() {
+        this.students = readFile(PATH_NAME);
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public boolean checkAgeAndGpa(int age, double gpa) {
+        return (age >= 18 && age <= 60 && !(gpa < 0) && !(gpa > 10));
+    }
+
+    public String getGenderByChoice(int choice) {
+        String gender = "";
+        switch (choice) {
+            case 1:
+                gender = "Male";
+                break;
+            case 2:
+                gender = "Female";
+                break;
+            case 3:
+                gender = "Other";
+                break;
+        } return gender;
+    }
+
+
+
+    public Student addStudent() {
         System.out.println("Nhap ho ten: ");
         String name = scanner.nextLine();
         System.out.println("Nhap tuoi: ");
         int age = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Nhap gioi tinh: ");
-        String gender = scanner.nextLine();
+        System.out.println("Chon gioi tinh: ");
+        System.out.println("1. Male");
+        System.out.println("2. Female");
+        System.out.println("3. Other");
+        System.out.println("Nhap lua chon: ");
+        int gender = scanner.nextInt();
+        scanner.nextLine();
         System.out.println("Nhap dia chi: ");
         String address = scanner.nextLine();
         System.out.println("Nhap diem trung binh: ");
         double gpa = scanner.nextDouble();
         scanner.nextLine();
-        return new Student(name, age, gender, address, gpa);
+        if (checkAgeAndGpa(age, gpa) && !getGenderByChoice(gender).equals("")) {
+            students.add(new Student(name, age, getGenderByChoice(gender), address, gpa));
+            writeFile(students, PATH_NAME);
+            System.out.println("Them sinh vien co ten = " + name + " thanh cong!");
+        } else {
+            System.out.println("Thong tin khong hop le, moi nhap lai!");
+        }
+        return new Student(name, age, getGenderByChoice(gender), address, gpa);
     }
 
-    public boolean addStudent (Student student) {
-        return students.add(student);
-    }
-
-    public Student updateStudent(String name) {
-        Student student = null;
-        for (Student s : students) {
-            if (s.getName().equals(name)) {
-                student = s;
+    public Student editStudent(String name) {
+        Student editStudent = null;
+        for (Student student : students) {
+            if (student.getName().equals(name)) {
+                editStudent = student;
             }
         }
-        if (student != null) {
+        if (editStudent != null) {
+            int index = students.indexOf(editStudent);
             System.out.println("Nhập tên mới: ");
-            String name1 = scanner.nextLine();
-            student.setName(name1);
+            editStudent.setName(scanner.nextLine());
             System.out.println("Nhập tuổi mới: ");
-            int age = scanner.nextInt();
-            student.setAge(age);
+            editStudent.setAge(scanner.nextInt());
+            scanner.nextLine();
             System.out.println("Nhap gioi tinh: ");
-            String gender = scanner.nextLine();
-            student.setGender(gender);
+            System.out.println("1. Male");
+            System.out.println("2. Female");
+            System.out.println("3. Other");
+            System.out.println("Nhap lua chon: ");
+            int gender = scanner.nextInt();
+            scanner.nextLine();
             System.out.println("Nhap dia chi moi: ");
-            String address = scanner.nextLine();
-            student.setAddress(address);
+            editStudent.setAddress(scanner.nextLine());
             System.out.println("Nhap diem trung binh moi: ");
-            double gpa = scanner.nextDouble();
-            student.setGpa(gpa);
+            editStudent.setGpa(scanner.nextDouble());
+            scanner.nextLine();
+            if (checkAgeAndGpa(editStudent.getAge(), editStudent.getGpa()) && !getGenderByChoice(gender).equals("")){
+                editStudent.setGender(getGenderByChoice(gender));
+                students.set(index, editStudent);
+                writeFile(students, PATH_NAME);
+                System.out.println("Sua thong tin ten = " + name + " thanh cong!");
+            } else {
+                System.out.println("Thong tin khong hop le, moi kiem tra lai");
+            }
         }
-        return student;
+        return editStudent;
     }
 
     public Student deleteStudent(String name) {
-        Student student = null;
-        for (Student s : students) {
-            if (s.getName().equals(name)) {
-                student = s;
+        Student editStudent = null;
+        for (Student student : students) {
+            if (student.getName().equals(name)) {
+                editStudent = student;
             }
         }
-        if (student != null) {
-            students.remove(student);
+        if (editStudent != null) {
+            students.remove(editStudent);
+            writeFile(students, PATH_NAME);
+            System.out.println("Xoa sinh vien co ten = " + name + " thanh cong!");
         }
-        return student;
+        return editStudent;
     }
 
-//    public Student displayStudent() {
-//        for (Student s : students) {
-//            boolean compareByNull = s != null;
-//            if (compareByNull) {
-//                System.out.println(s);
-//            }
-//        }
-//        return students;
-//    }
-
-    public void displayStudent(){
-        students.forEach(System.out::println);
+    public void displayAll(){
+        if (students.isEmpty()) {
+            System.out.println("Danh sach sinh vien trong");
+            return;
+        }
+        for (Student student: students) {
+            System.out.println(student);
+        }
     }
 
-    public void averagePointHigher(){
-        List<Student> students = new ArrayList<>();
-        for (Student s : students) {
-            if(s.getGpa() >= 7.5){
-                students.add(s);
+    public void displayStudentByGpa(){
+        if (students.isEmpty()) {
+            System.out.println("Danh sach sinh vien trong!");
+            return;
+        }
+        for (Student student : students) {
+            if(student.getGpa() > 7.5){
+                System.out.println(student);;
             }
         }
-        students.forEach(System.out::println);
     }
+
+    public void displayStudentFormat() {
+        if (students.isEmpty()) {
+            System.out.println("Danh sach sinh vien trong");
+            return;
+        }
+        for (Student student : students) {
+            if (student.getGpa() > 8.0) {
+                System.out.println(student.getName() + " - " + student.getGpa() + " - " + "Good");
+            } else if (student.getGpa() > 6.0) {
+                System.out.println(student.getName() + " - " + student.getGpa() + " - " + "Pretty");
+            }else if (student.getGpa() > 4.0) {
+                System.out.println(student.getName() + " - " + student.getGpa() + " - " + "Normal");
+            } else {
+                System.out.println(student.getName() + " - " + student.getGpa() + " - " + "Weak");
+            }
+        }
+    }
+
+    public void writeFile(ArrayList<Student> students, String path) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path));
+            for (Student student : students) {
+                bufferedWriter.write(student.getName() + "," + student.getAge() + "," + student.getGender() + "," + student.getAddress() + "," + student.getGpa() + "\n");
+            }
+            bufferedWriter.close();
+            System.out.println("Write file thanh cong");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public ArrayList<Student> readFile(String path) {
+        ArrayList<Student> students = new ArrayList<>();
+        File file = new File(PATH_NAME);
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] strings = line.split(",");
+                students.add(new Student(strings[0], Integer.parseInt(strings[1]), strings[2], strings[3], Double.parseDouble(strings[4])));
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return students;
+    }
+
 }
