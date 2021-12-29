9700351bd8feac5e1fed57513b2d6ecaac0a35b4
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2021-12-29 09:17:24 +0700

    up regex email

---
+++ Exercise/src/Bai3Library/model/Student.java
@@ -0,0 +1,60 @@
+package Bai3Library.model;
+
+public class Student {
+    private String name;
+    private int id;
+    private String dateOfBirth;
+    private String className;
+
+    public Student() {
+    }
+
+    public Student(String name, int id, String dateOfBirth, String className) {
+        this.name = name;
+        this.id = id;
+        this.dateOfBirth = dateOfBirth;
+        this.className = className;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", className='" + className + '\'' +
+                '}';
+    }
+}
